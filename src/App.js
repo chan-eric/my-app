d36d2b3809a7eac2a37751ef300d8cba602d50b4
Eric Chan <ericchan@Erics-MacBook-Pro.local>
2018-05-18 16:13:54 -0400

    nav bar v1

--- src/App.js
+++ src/App.js
@@ -1,43 +1,41 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Link, Switch, Route } from 'react-router-dom'
-import './css/App.css';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import "./css/App.css";
+import logo from "./logo.svg";
 import Table from "./Table";
 import Contact from "./Contact";
 
-
 class App extends Component {
-    constructor(props) {
-      super(props);
-    }
-	
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
-    <BrowserRouter>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
 
-          <div className="Menubar">
-            <ul className="Menu">
-              <li><Link to='/'>Home</Link></li>
-              <li><Link to='/contact'>Contact Us</Link></li>
-            </ul> 
+            <div className="Menubar">
+              <ul className="Menu">
+                <li className={window.location.pathname === "/" && "selected"}>
+		<Link to="/">Home</Link>
+                </li>
+				<li className={window.location.pathname === "/contact" && "selected"}>
+                  <Link to="/contact">Contact Us</Link>
+                </li>
+              </ul>
+            </div>
+          </header>
+
+          <div className="App-intro">
+            <div className="Banner" />
+            <Route exact path="/" component={Table} />
+            <Route path="/contact" component={Contact} />
           </div>
-        </header>
-		
-      
-		
-		  <div className="App-intro">
-
-        <div className="Banner"></div>
-          <Route exact path='/' component={Table}/>
-          <Route path='/contact' component={Contact}/>	
-		  
-		  </div>
-    </div>
-    </BrowserRouter>
+        </div>
+      </BrowserRouter>
     );
   }
 }
