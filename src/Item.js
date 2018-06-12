d9311f41ffe0aada2df03b177ab3b637e4458d3b
Eric Chan <ericchan@Erics-MacBook-Pro.local>
2018-06-12 00:43:05 -0400

    modified item

--- src/Item.js
+++ src/Item.js
@@ -8,8 +8,9 @@ const Item = props => (
         <img src={props.data.img} alt="Smiley face" />
       </td>
       <td valign="top">
-        <h2>{props.data.name}</h2>
-        {props.data.description}
+        <h2>{props.data.name} </h2>
+		<h4> 价格 {props.data.price} </h4>
+		{props.data.description}
       </td>
     </tr>
   </div>
